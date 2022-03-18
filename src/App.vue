ec42572c9e9813e8ba429421f39ba7295c03698c
Miracle-omosede <miracleigbinoghene@gmail.com>
2022-03-18 03:23:14 +0100

    simple portfolio app

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <!-- <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/> -->
+  <div>
+    <Home />
+  </div>
+</template>
+
+<style>
+  *{
+    font-family: 'Nunito', sans-serif;
+  }
+</style>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
